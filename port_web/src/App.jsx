c5a64cab7456899448fb223a6c1d973f1420ad96
piyushbijarania <piyush.gag6513@gmail.com>
2025-08-16 19:56:02 +0530

    initialised react project

---
+++ port_web/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="h-screen w-full bg-blue-500 text-black text-4xl">
+        hkdgfkasdkjfh
+      </div>
+        
+    </>
+  )
+}
+
+export default App
